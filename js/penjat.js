b7d1ebc8d2f8d7dd15760f6e17355fd4cbb9ab63
frangaar <dnerizb2223@politecnics.barcelona>
2024-02-29 19:54:11 +0100

    casi casi

--- js/penjat.js
+++ js/penjat.js
@@ -1,33 +1,28 @@
 
 document.addEventListener("DOMContentLoaded",()=>{
 
-
 const words = ["javascript", "html", "css", "python", "typescript"];
-const maxWrongAttempts = 10;
-let selectedWord, guessedWord, wrongAttempts,gameEnded;
-
+const maxWrongAttempts = 9;
+let gameEnded = false;
+let selectedWord, guessedWord, wrongAttempts, usedLetters;
 
 if (localStorage.getItem("selectedWord")== undefined) {
     // SI ES LA PRIMERA VEGADA
     selectedWord = words[Math.floor(Math.random() * words.length)];
     localStorage.setItem("selectedWord", selectedWord);
-    guessedWord = Array.from({ length: selectedWord.length }, () => "_").join(" ");
+    guessedWord = Array.from({ length: selectedWord.length }, () => "_");
     localStorage.setItem("guessedWord", guessedWord);
     wrongAttempts = 0;
     localStorage.setItem("wrongAttempts", wrongAttempts);
-    gameEnded = false;
-    localStorage.setItem("gameEnded", gameEnded);
-
-}else{
-    // SI JA ESTAVES JUGANT
-    selectedWord=localStorage.getItem("selectedWord");
-    guessedWord=localStorage.getItem("guessedWord");
-    wrongAttempts=localStorage.getItem("wrongAttempts");
-    gameEnded=localStorage.getItem("gameEnded");
-    document.getElementById("word-display").innerText = guessedWord;
-
+}else {
+    // SI JA ESTAVES JUGANT f5
+    selectedWord = localStorage.getItem("selectedWord");
+    guessedWord = localStorage.getItem("guessedWord").split(",");
+    wrongAttempts = localStorage.getItem("wrongAttempts");
+    usedLetters = localStorage.getItem("usedLetters") ? JSON.parse(localStorage.getItem("usedLetters")) : [];
+    document.getElementById("wrong-count").innerText = wrongAttempts;
+    document.getElementById("word-display").innerText = guessedWord.join(" ");
 }
-
 
 initializeGameInterface();
 
@@ -38,18 +33,62 @@ function updateGameInterface() {
     if (wrongAttempts >= maxWrongAttempts) {
         displayMessage("¡Has perdido! La palabra correcta era: " + selectedWord);
         gameEnded = true;
-        localStorage.clear();
+        localStorage.clear();e89
     }
 }
 
+let img = document.createElement("img");
 function ensenyarNinot() {
-        let img = document.createElement("img");
-        img.src = "./media/1.png"; 
-        document.getElementById("image-container").appendChild(img);
+    switch (wrongAttempts) {
+        case 1:
+            img.src = "./media/1.png"; 
+        break;
+        case 2:
+            img.src = "./media/2.png"; 
+        break;
+        case 3:
+            img.src = "./media/3.png"; 
+        break;
+        case 4:
+            img.src = "./media/4.png"; 
+        break;
+        case 5:
+            img.src = "./media/5.png"; 
+        break;
+        case 6:
+            img.src = "./media/6.png"; 
+        break;
+        case 7:
+            img.src = "./media/7.png"; 
+        break;
+        case 8:
+            img.src = "./media/8.png"; 
+        break;
+        case 9:
+            img.src = "./media/9.png"; 
+        break;
+        default:
+            break;
+    }
+    document.getElementById("image-container").appendChild(img);
 }
+
+
 
 function initializeGameInterface() {
     document.getElementById("word-display").innerText = guessedWord.join(" ");
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyzçñ";
+    for (const letter of alphabet) {
+        const button = document.getElementById(`btn-${letter}`);
+        if (button) {
+            button.disabled = false;
+        }
+    }
+}
+
+function disableButton(letter) {
+    document.getElementById(`btn-${letter}`).disabled = true;
 }
 
 function displayMessage(message) {
@@ -57,16 +96,19 @@ function displayMessage(message) {
 }
 
 function guessLetter(letter) {
-    if (gameEnded) {
-        return; 
+    if (gameEnded || usedLetters.includes(letter)) {
+        return;
     }
 
     letter = letter.toLowerCase();
+    usedLetters.push(letter);
+    localStorage.setItem("usedLetters", JSON.stringify(usedLetters));
 
     if (!selectedWord.includes(letter)) {
         wrongAttempts++;
         localStorage.setItem("wrongAttempts", wrongAttempts);
         updateGameInterface();
+        ensenyarNinot();
     } else {
         for (let i = 0; i < selectedWord.length; i++) {
             if (selectedWord[i] === letter) {
@@ -75,17 +117,18 @@ function guessLetter(letter) {
         }
 
         localStorage.setItem("guessedWord", guessedWord);
-        
+
         updateGameInterface();
 
-        if (guessedWord.join(" ") === selectedWord) {
+        if (guessedWord.join("") === selectedWord) {
             displayMessage("¡Felicidades! Has adivinado la palabra correctamente.");
             gameEnded = true;
             localStorage.clear();
+        } else {
+            disableButton(letter);
         }
     }
 }
-
 
 
 document.addEventListener("keydown", function (event) {
